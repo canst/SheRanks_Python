e8f0f1040c8ae2973320df0b11d228fb8b839111
canst <47247401+canst@users.noreply.github.com>
2025-08-02 13:35:18 +0100

    Fix bug to single university individual ranking + delete a csv file

--- universities/urls.py
+++ universities/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.university_list, name='list'),
     path('<slug:university_slug>/', views.university_detail, name='detail'),
     path('<slug:university_slug>/rate/', views.rate_university, name='rate'),
+    # This is the line we're adding back in.
+    path('<slug:university_slug>/post/', views.create_post, name='create_post'),
 ]
